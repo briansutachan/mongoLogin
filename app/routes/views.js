1f204b075e25eec80c5dad7b6e89220a8b8b5d02
briansutachan <bsutachan@techlaunch.io>
2018-04-04 17:44:42 -0400

    first commit

---
+++ app/routes/views.js
@@ -0,0 +1,65 @@
+module.exports = function (app) {
+
+
+    app.get('/', function (req, res) {
+        if (req.user) {
+            res.redirect('/profile');
+        }
+        else {
+            res.render('index.ejs'); // load the index.ejs file
+        }
+    });
+
+
+    app.get('/login', function (req, res) {
+
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+
+    app.get('/signup', function (req, res) {
+
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/password-recovery', function (req, res) {
+        res.render('password_recovery.ejs', { message: req.flash('passwordRecoveryMessage') });
+    });
+
+    app.get('/update-profile', isLoggedIn, function (req, res) {
+        res.render('update_profile.ejs', {
+            user: req.user,
+            message: req.flash('updateProfileMessage')
+        });
+    });
+
+    app.get('*', function (req, res) {
+        res.render('404.ejs');
+    });
+
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
